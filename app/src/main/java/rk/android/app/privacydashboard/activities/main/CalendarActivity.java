a102618fa11b1783171a347c2d0ac79658a740f9
gphillc <47837968+gphillc@users.noreply.github.com>
2021-11-14 21:56:20 -0500

    anything

---
+++ app/src/main/java/rk/android/app/privacydashboard/activities/main/CalendarActivity.java
@@ -0,0 +1,13 @@
+package rk.android.app.privacydashboard.activities.main;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity {
+
+    public class SettingsActivity extends AppCompatActivity {
+
+        Context context;
+    }
+}
